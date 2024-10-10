33c0bfb577b0c8fb33aaa7af85f68de77aded39b
Deivid30Medina <deivis32sebas02@gmail.com>
2024-10-10 12:58:16 -0500

    fix(header): Fixing header animation

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import IconMenuBurger from '../../../assets/icons/IconMenuBurger';
 import IconDNDA from '../../../assets/icons/IconDNDA';
-import NavItem from './NavItem'; 
-import ExternalNavItem from './ExternalNavItem'; 
+import NavItem from './NavItem';
+import ExternalNavItem from './ExternalNavItem';
 import '../../../styles/MediaQuerys.css';
 import IconMinInterior from '../../../assets/icons/IconMinInterior';
 
@@ -32,9 +32,10 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`w-full bg-color-dnda flex items-center justify-between z-50 custom-max-h 
-        ${isAtTop ? 'h-40 flex-col pt-3 transition-all duration-1000' : 'fixed top-0 left-0 flex-row min-h-16 transition-all duration-1000 px-8'}`}
+      className={`w-full bg-color-dnda flex items-center justify-between z-50 custom-max-h transition-all duration-1000 ease-in-out
+    ${isAtTop ? 'max-h-40 flex-col pt-3' : 'fixed top-0 left-0 flex-row max-h-16 px-8'}`}
     >
+
       <a href="https://www.derechodeautor.gov.co/es" target='_blank' className={`flex items-center justify-center ${isAtTop ? 'block' : 'hidden'}`}>
         <IconDNDA className={`cursor-pointer transition-opacity duration-300 h-auto w-32 xl:w-48`} />
       </a>
@@ -47,8 +48,8 @@ const Navbar = () => {
         <IconMenuBurger width={35} height={35} fill='red' className='cursor-pointer' />
       </div>
 
-      <ul className={`absolute w-full bg-color-dnda flex flex-col items-center justify-center z-50 top-16 left-0 transition-all duration-700 ease-in-out overflow-hidden gap-12 ${isMenuOpen ? (isAtTop ? 'max-h-[550px] mt-12 opacity-100' : 'mt-0 opacity-100') : 'max-h-0 opacity-0'} xl:flex xl:flex-row xl:static xl:top-0 xl:bg-transparent xl:w-auto xl:max-h-none xl:opacity-100`}>
-      <NavItem to='/' onClick={() => setIsMenuOpen(false)}>Inicio</NavItem>
+      <ul className={`absolute w-full bg-color-dnda flex flex-col items-center justify-center z-50 top-16 left-0 transition-all duration-700 ease-in-out overflow-hidden gap-12 ${isMenuOpen ? (isAtTop ? 'max-h-[550px] mt-10 opacity-100' : 'mt-0 opacity-100') : 'max-h-0 opacity-0'} xl:flex xl:flex-row xl:static xl:top-0 xl:bg-transparent xl:w-auto xl:max-h-none xl:opacity-100`}>
+        <NavItem to='/' onClick={() => setIsMenuOpen(false)}>Inicio</NavItem>
         <NavItem to='/informes' onClick={() => setIsMenuOpen(false)}>Informes</NavItem>
         <ExternalNavItem href="#" >Inscripción</ExternalNavItem>
         <ExternalNavItem href="#" >Reglamento</ExternalNavItem>
